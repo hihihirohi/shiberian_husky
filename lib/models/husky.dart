fdfd22b40a58108bef1a11c48db23260fdbc5f98
hihihirohi <hirothugu326@gmail.com>
2020-07-06 18:41:08 +0900

    モデルの追加

---
+++ lib/models/husky.dart
@@ -0,0 +1,40 @@
+class Husky {
+  final int id, age, price;
+  final String name, sex, color, address, image;
+
+  Husky({this.id, this.age, this.price, this.name, this.sex, this.color, this.address, this.image});
+}
+
+
+List<Husky> huskys =[
+  Husky(
+    id: 1,
+    age: 1,
+    price: 3000000,
+    name: "Husky",
+    sex: "男",
+    color: "Black",
+    address: "Osaka",
+    image: "assets/images/husky.png"
+  ),
+  Husky(
+      id: 2,
+      age: 1,
+      price: 3000000,
+      name: "Husky",
+      sex: "男",
+      color: "Black",
+      address: "Osaka",
+      image: "assets/images/husky.png"
+  ),
+  Husky(
+      id: 3,
+      age: 1,
+      price: 3000000,
+      name: "Husky",
+      sex: "男",
+      color: "Black",
+      address: "Osaka",
+      image: "assets/images/husky.png"
+  ),
+];
